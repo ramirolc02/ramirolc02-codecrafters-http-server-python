5d97d76d2e988cb7eff74dc5d82f7ffb87ea4ad1
Rama Lopez Cento <ramirolopezcento@icloud.com>
2024-07-12 00:06:42 +0200

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -7,11 +7,15 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, address = server_socket.accept() # wait for client
-    receive = connection.recv(1024) # receive data
-    if receive.split()[0] == b"GET / HTTP/1.1":
-        connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    else:
-        connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+    req = connection.recv(1024).decode() # receive data
+    data = req.split("\r\n")
+
+    response = "HTTP/1.1 200 OK\r\n\r\n".encode()
+    if data[0].split(" ")[1] != "/":
+        response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()   
+    connection.send(response) # send response 
+    connection.close() # close the connection after sending the response
 
 if __name__ == "__main__":
     main()
