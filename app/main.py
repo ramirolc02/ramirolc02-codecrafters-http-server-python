04ea551ce4cf5b248a681dff351426af9886d3cf
Rama Lopez Cento <ramirolopezcento@icloud.com>
2024-07-12 23:31:13 +0200

    STAGE 5

--- app/main.py
+++ app/main.py
@@ -11,12 +11,15 @@ def main():
     req = connection.recv(1024).decode() # receive data
     data = req.split("\r\n")
     endpoint = data[0].split(" ")[1]
-
+   
     if endpoint == "/":
         response = "HTTP/1.1 200 OK\r\n\r\n".encode()
     elif endpoint.startswith("/echo/") :
         string = data[0].split(" ")[1].split("/")[2]
         response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}'.encode()
+    elif endpoint == "/user-agent":
+        userAgent = data[3].split(": ")[1]
+        response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(userAgent)}\r\n\r\n{userAgent}'.encode()
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()   
     
